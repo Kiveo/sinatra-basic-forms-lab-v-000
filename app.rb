7f94e9a3917f10dab792c55e65a432a92ae9ff12
Maverick Tesini <mavericktesini@gmail.com>
2018-07-14 07:37:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,6 +11,10 @@ class App < Sinatra::Base
   end
 
   post '/display_puppy' do
+    @pup = Puppy.new
+    @pup.name = params[:name]
+    @pup.age = params[:age]
+    @pup.breed = params[:breed]
     erb :display_puppy
   end
 
